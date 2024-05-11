ad9d9c2bb7f3aa214f9b625b11348b7c348b16a0
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-05-11 17:43:12 -0500

    Se añadió el middleware para devolver un archivo html, pero no funciona con los paths relativos

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 //npm init -y
-
 //npm install express -E
 
 const express = require('express');
 const app = express();
-
+const path = require('path');
 const PORT = process.env.PORT ?? 3000;
-app.get(('/'), (req, res) => {
-  res.status(200).send('<h1>Hola Mundo</h1>');
+
+
+app.disable('x-powered-by');
+
+
+//solicitudes
+
+//app.get(('/'), (req, res) => {
+//  res.status(200).send('<h1>Hola Mundo</h1>');
+//});
+
+app.use((req, res) => {
+  res.status(400).sendFile(path.join(__dirname, 'pages/dashboard.html'));
 });
+
+
+
+
 app.listen(PORT, () => {
   console.log(`escuchando en puerto: http://localhost:${PORT}`);
 });
