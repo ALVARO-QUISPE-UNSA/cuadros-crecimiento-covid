a408e29f4a4ee5209f3003f86a82c8aa42215614
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-05-11 16:29:28 -0500

    Se añadó index del sevidor

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT ?? 3000;
+app.get(('/'), (req, res) => {
+  res.status(200).send('<h1>Hola Mundo</h1>');
+});
+app.listen(PORT, () => {
+  console.log(`escuchando en puerto: http://localhost:${PORT}`);
+});
