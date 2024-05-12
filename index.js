6f26fd0890ba1a29d24a0270c82cac71757c04fa
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-05-12 07:43:41 -0500

    se logró modificar los paths para mostrar archivos estáticos

--- index.js
+++ index.js
@@ -16,12 +16,15 @@ app.disable('x-powered-by');
 //  res.status(200).send('<h1>Hola Mundo</h1>');
 //});
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/prueba.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public/problema1/prueba.html'));
 });
 
+app.get('/p1', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public/problema1/dashboard.html'));
+});
 
 
 
